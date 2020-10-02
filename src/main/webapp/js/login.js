5931676e1207f4da65a964084aa842a962f0d9a5
60rzvvbj <1808078515@qq.com>
2020-10-02 22:16:48 +0800

    测试获取用户信息

--- src/main/webapp/js/login.js
+++ src/main/webapp/js/login.js
@@ -1,3 +1,4 @@
+// 登录
 var login = getDQS('.login');
 var loginInput = getDQSA('input', login);
 var loginEmail = loginInput[0];
@@ -20,3 +21,39 @@ loginSubmit.addEventListener('click', function () {
         }
     })
 });
+// 注册
+var register = getDQS('.register');
+var registerInput = getDQSA('input', register);
+var registerUserName = registerInput[0]; // 用户名
+var registerEmail = registerInput[1]; // 邮箱
+var registerVerificationCode = registerInput[2]; //输入验证码
+var registerGetVerificationCode = registerInput[3]; //获取验证码
+var registerPassword = registerInput[4]; //输入密码
+var registerConfirmPassword = registerInput[5]; //确认密码
+var registerSubmit = registerInput[6]; // 提交
+var registerRealVerificationCode; //后端返回的验证码
+
+/**
+ * 判断表单填写验证等是否合法，若不合法则给出相对应的处理
+ */
+function registerJudge() {
+    return true;
+}
+registerEmail.addEventListener('blur', function () {
+    console.log('判断邮箱是否存在，是否被注册过');
+});
+registerGetVerificationCode.addEventListener('click', function () {
+    console.log('获取验证码');
+});
+registerSubmit.addEventListener('click', function () {
+    if (registerJudge()) {
+        ajax({
+            type: 'post',
+            url: '/user',
+            data: {},
+            success: function (res) {
+
+            }
+        });
+    }
+})
