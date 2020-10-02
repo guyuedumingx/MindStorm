fa2092e1ff1ee30386f85d750ef2869677c0dce7
guyuedumingx <guyuedumingx@gmail.com>
2020-10-02 22:37:28 +0800

    userController

--- src/main/java/controller/UserController.java
+++ src/main/java/controller/UserController.java
@@ -22,6 +22,8 @@ public class UserController extends BaseController{
     public void login(HttpServletRequest request, HttpServletResponse response) throws IOException {
         String email = request.getParameter("email");
         String password = request.getParameter("password");
+        response.setHeader("Access-Control-Allow-Origin","*");
+        response.setHeader("Cache-Control","no-cache");
         UserService service = new UserServiceImpl();
         int login = service.login(email, password);
         WebUtil.renderText(response,login+"");
@@ -35,6 +37,7 @@ public class UserController extends BaseController{
     public void getUserInfo(HttpServletRequest request, HttpServletResponse response) throws IOException {
         int id =Integer.valueOf(request.getParameter("user_id"));
         boolean isAuthor = Boolean.valueOf(request.getParameter("is_author"));
+        response.setHeader("Access-Control-Allow-Origin","*");
         if(isAuthor){
             User user = new UserServiceImpl().getUser(id);
             WebUtil.renderJson(response,user);
