3216f53fc770343bb16160d02b9f982e3abae791
Tracy <1030970578@qq.com>
2020-10-07 10:23:14 +0800

    尝试点击空白处消失

--- src/main/webapp/js/index.js
+++ src/main/webapp/js/index.js
@@ -2,6 +2,7 @@
 // 引入工具类js
 document.write("<script language='javascript' src='js/concise.js'></script>");
 
+
 window.onload = function () {
     // 获取快捷键按钮
     var shortcut = getDQS(".shortcutKey");
@@ -9,8 +10,21 @@ window.onload = function () {
     var help = getDQS('.help');
     //获取快捷键框
     var shotcutNav = getDQS(".shortcut_nav");
-
     shortcut.addEventListener("click", function () {
         shotcutNav.style.display = "block";
     })
+    // 点击消失
+    document.onclick = function (event) {
+        var event = event || window.event;
+
+        var targetId = event.target ? event.target.id : event.srcElement.id;
+
+        // 看明白这个写法
+
+        if (targetId != "shortcutKey")  // 不等于当前点点击的名字
+
+        {
+            shotcutNav.style.display = "block";
+        }
+    }
 }
