ad8b7f1a21cec692b13c334c87173736b802b2ba
guyuedumingx <guyuedumingx@gmail.com>
2020-10-13 12:54:50 +0800

    fix register

--- src/main/java/controller/RegisterController.java
+++ src/main/java/controller/RegisterController.java
@@ -5,6 +5,7 @@ import common.util.WebUtil;
 import pojo.User;
 import service.UserService;
 import service.impl.UserServiceImpl;
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -25,10 +26,16 @@ public class RegisterController extends BaseController {
      */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setHeader("Access-Control-Allow-Origin","*");
         User usr = WebUtil.getJson(request, User.class);
         UserService userService = new UserServiceImpl();
         User register = userService.register(usr);
         int i = StatusCode.nullObjcet(register);
         WebUtil.renderMap(response,"status_code",i+"");
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        this.doPost(request, response);
     }
 }
