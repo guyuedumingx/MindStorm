827a3ce2f3495ee91d46f5a16b3b711be5e4bde2
guyuedumingx <guyuedumingx@gmail.com>
2020-10-15 20:27:36 +0800

    add cookie

--- src/main/java/controller/AvatarController.java
+++ src/main/java/controller/AvatarController.java
@@ -3,11 +3,8 @@ package controller;
 import pojo.User;
 import service.UserService;
 import service.impl.UserServiceImpl;
-
 import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.*;
 import java.io.*;
 
@@ -15,8 +12,8 @@ import java.io.*;
  * 负责用户头像
  * @author yohoyes
  */
-@MultipartConfig
 @WebServlet("/user/avatar")
+@MultipartConfig
 public class AvatarController extends BaseController {
     private static final long serialVersionUID = 1L;
 
@@ -28,16 +25,14 @@ public class AvatarController extends BaseController {
      */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        Part part = request.getPart("image");
-        String fileName = getFileName(part);
-        String realPath = request.getSession().getServletContext().getRealPath("")+"img/";
-        mkDir(realPath);
-        writeTo(realPath+fileName,part);
-        UserService service = new UserServiceImpl();
-        HttpSession session = request.getSession();
-        User user = (User)session.getAttribute("user");
-        service.setUserImage(user.getUser_id(),fileName);
-        session.setAttribute("user",service.getUser(user.getUser_id()));
+//        Part part = request.getPart("image");
+//        String fileName = getFileName(part);
+//        String realPath = request.getSession().getServletContext().getRealPath("")+"img/";
+//        mkDir(realPath);
+//        writeTo(realPath+fileName,part);
+//        UserService service = new UserServiceImpl();
+//        HttpSession session = request.getSession();
+//        User user = (User)session.getAttribute("user");
     }
 
     /**
@@ -49,13 +44,14 @@ public class AvatarController extends BaseController {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         this.doPost(request, response);
-
     }
+
     private String getFileName(Part part) {
         String head = part.getHeader("Content-Disposition");
         String fileName = head.substring(head.indexOf("filename=\"") + 10, head.lastIndexOf("\""));
         return fileName;
     }
+
     private void writeTo(String fileName, Part part)throws IOException {
         InputStream in = part.getInputStream();
         OutputStream out = new FileOutputStream(fileName);
@@ -67,6 +63,7 @@ public class AvatarController extends BaseController {
         in.close();
         out.close();
     }
+
     private void mkDir(String path){
         File file = new File(path);
         if(!file.exists()){
