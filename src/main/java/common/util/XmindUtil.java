ac59d44dfecdbe32414042c032666260c85a1e53
guyuedumingx <guyuedumingx@gmail.com>
2020-10-16 12:00:29 +0800

    update xmindUtil

--- src/main/java/common/util/XmindUtil.java
+++ src/main/java/common/util/XmindUtil.java
@@ -1,25 +1,6 @@
 package common.util;
 
 
-import org.xmind.core.*;
-
-import java.io.IOException;
-
 public class XmindUtil {
-    IWorkbookBuilder builder = Core.getWorkbookBuilder();//初始化builder
-    IWorkbook workbook = null;
-    
-   static {
-       try {
-           workbook = builder.loadFromPath(path);//打开XMind文件
-       } catch (
-               IOException e) {
-           e.printStackTrace();
-       } catch (
-               CoreException e) {
-           e.printStackTrace();
-       }
-       ISheet defSheet = workbook.getPrimarySheet();//获取主Sheet
-       rootTopic = defSheet.getRootTopic(); //获取根Topic
-   }
+
 }
