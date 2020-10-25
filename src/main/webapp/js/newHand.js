86a6afb774e501046fc2808d779adaf8a34899e0
60rzvvbj <1808078515@qq.com>
2020-10-25 19:09:40 +0800

    新手页面新增how和页脚

--- src/main/webapp/js/newHand.js
+++ src/main/webapp/js/newHand.js
@@ -2,7 +2,9 @@ var w4 = getDom('.w4').children;
 for (var i = 0; i < w4.length; i++) {
     if (i & 1) {
         w4[i].style.flexFlow = 'row-reverse nowrap';
+        // w4[i].getDom('.tag').style.textAlign = 'left';
     } else {
         w4[i].style.flexFlow = 'row nowrap';
+        // w4[i].getDom('.tag').style.textAlign = 'right';
     }
 }
