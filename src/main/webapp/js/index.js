35875926723f7edf5556c68ddfbb0a37ff361d24
Tracy <1030970578@qq.com>
2020-11-15 10:11:53 +0800

    进度条渐变

--- src/main/webapp/js/index.js
+++ src/main/webapp/js/index.js
@@ -80,7 +80,7 @@ function userMess(head, headBox, emailBox, perSig) {
             perSig.value = userIntroduce;
             addLiBox(userProjectLength, userProject, personalNav)
         },
-        error: function () { }
+        error: function () {}
     });
 
 
@@ -409,7 +409,7 @@ function butStyle(projectNav, liArr, x) {
         });
     }
 }
-
+var ddlColor = new getGradientColor("rgb(6,41,46)", "rgb(255,0,0)");
 //项目添加---------
 function addLi(li, name, introduce, author, number, projectID, projectTime) {
     var divName = document.createElement("div");
@@ -443,9 +443,11 @@ function addLi(li, name, introduce, author, number, projectID, projectTime) {
     divIn.appendChild(spanTxt);
     li.appendChild(divDdl);
     // divDdl.style.padding = "0";
-    if (projectTime >= 100)
+    if (projectTime > 100)
         projectTime = 100;
     divDdl.style.width = projectTime + "%";
+    divDdl.style.backgroundColor = ddlColor.get(projectTime / 100);
+    	console.log(ddlColor.get(projectTime / 100));
     li.appendChild(divBot);
     divBot.appendChild(divAut);
     divAut.appendChild(spanName);
@@ -455,8 +457,8 @@ function addLi(li, name, introduce, author, number, projectID, projectTime) {
     li.addEventListener("click", function () {
         window.location = "project.html?project_id=" + projectID;
     })
-
 }
+
 //删除内容
 function removeLi() {
     var searchNav = getDom(".searchNav");
@@ -682,23 +684,4 @@ start();
 // 计算进度条的函数
 function jdt(createTime, deadline) {
     return (1 - (deadline - Date.now()) / (deadline - createTime)) * 100;
-}
-
-// 根据百分比获取渐变颜色
-class getGradientColor {
-    constructor(startColor, endColor) {
-        startColor = startColor.replace(/ /i, '');
-        endColor = endColor.replace(/ /i, '');
-        var startColorArr = startColor.split('(')[1].split(')')[0].split(',');
-        var endColorArr = endColor.split('(')[1].split(')')[0].split(',');
-        this.sr = startColorArr[0] - 0;
-        this.sg = startColorArr[1] - 0;
-        this.sb = startColorArr[2] - 0;
-        this.er = endColorArr[0] - 0;
-        this.eg = endColorArr[1] - 0;
-        this.eb = endColorArr[2] - 0;
-        this.get = function (percentage) {
-            return 'rgb(' + (this.sr + (this.er - this.sr) * percentage) + ',' + (this.sg + (this.eg - this.sg) * percentage) + ',' + (this.sb + (this.eb - this.sb) * percentage) + ')';
-        }
-    }
 }
